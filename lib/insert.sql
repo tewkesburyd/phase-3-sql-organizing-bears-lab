bb5cda03caa3e2f4007801c684660b329a6f7869
David Tewkesbury <tewkesburyd@hotmail.com>
2022-12-15 18:23:08 -0700

    commit tree 0df647fb0cc22814fd3c4f1aeb45fe464075de51

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "M", "Brown", "Lazy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, "M", "Brown", "Lazy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 3, "M", "Brown", "Lazy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 4, "M", "Brown", "Lazy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 5, "M", "Brown", "Lazy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 6, "M", "Brown", "Lazy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 7, "M", "Brown", "Lazy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, 8, "M", "Brown", "Lazy", 1);
